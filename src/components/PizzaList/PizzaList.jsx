c4f7dc864cf64db14db4f642026c639a0c517c8d
Andrii-Smolianinov <andr380v@gmail.com>
2023-09-07 22:39:43 +0300

    correct PizzaItem

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,9 +1,6 @@
 import PizzaItem from "./PizzaItem";
 
 export default function PizzaList() {
-
- 
-
   return (
     <ul className="p-[40px] flex flex-wrap gap-8 justify-center">     
       <PizzaItem />
