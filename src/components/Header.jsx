35bde1fc17995f4474299de0ab6606f2657d9264
Andrii-Smolianinov <andr380v@gmail.com>
2023-09-15 09:55:34 +0300

    тест коду Планування задач (add, edit. remove) у pages/Cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="flex bg-yellow-100 border-b-2 border-red-600">
       <Link to="/" className="cursor-pointer flex items-center">
-        <img src="images/logo.jpg" alt="logo" className="w-16 h-15" />
+        <img src="images/favicon.ico" alt="logo" className="w-16 h-15 m-1" />
         <span className="uppercase font-black italic text-xl drop-shadow-lg ml-2">
           <span className="text-lime-600">rea</span>
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-lime-600 to-red-600">ctpi</span>
