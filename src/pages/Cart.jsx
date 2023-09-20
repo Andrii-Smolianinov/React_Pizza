e899642a7d0edd78698bd7d7d4285924e454daf8
Andrii-Smolianinov <97645771+Andrii-Smolianinov@users.noreply.github.com>
2023-09-20 21:39:45 +0300

    update Cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,39 +1,4 @@
 import React from "react";
 export default function Cart() {
-  const [tasks, setTasks] = React.useState([{ text: "new task" }]);
-  const onClickButton = function () {
-    const text = prompt("add task");    
-    setTasks([...tasks, { text }]);
-  };
-
-  const onClickRemove = function (index) {
-    setTasks(tasks.filter((_, i) => i !== index));
-  };
-
-  const onClickEdit = function (index) {
-    const text = prompt("edit task");
-    setTasks(
-      tasks.map((obj, i) => {
-        if (index === i) {
-          return { ...obj, text: text };
-        }
-        return obj;
-      })
-    );
-  };
-
-  const element = tasks.map((task, index) => (
-    <li key={index} className="border inline-flex m-2 p-1">
-      {task.text}
-      <button onClick={() => onClickRemove(index)} className="border">X</button>
-      <button onClick={() => onClickEdit(index)} className="border">edit</button>
-    </li>
-  ));
-
-  return (
-    <>
-      <ul>{element}</ul>
-      <button onClick={onClickButton} className="border">add task</button>
-    </>
-  );
+  return <>cart</>;
 }
