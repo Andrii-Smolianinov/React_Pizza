1ce1212a88ae45534639adbe22a2dd7271d1a0be
Andrii-Smolianinov <97645771+Andrii-Smolianinov@users.noreply.github.com>
2023-09-30 00:33:57 +0300

    correct adaptive style

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -5,11 +5,11 @@ import Skeleton from "./PizzaItem/Skeleton";
 
 export default function PizzaList({ items, isLoading }) {
   return (
-    <ul className="flex flex-wrap justify-center
+    <ul className="flex flex-wrap justify-center min-h-[65vh]
     p-2 sm:p-3 lg:p-8 
     gap-3 sm:gap-6 lg:gap-8">
       {isLoading ? (
-        [...new Array(10)].map((_, index) => <Skeleton key={index} />)
+        [...new Array(5)].map((_, index) => <Skeleton key={index} />)
       ) : (
         <PizzaItem items={items} />
       )}
