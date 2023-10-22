916750bfd5e9fbb5ac152cb4a0c01d3876009614
Andrii-Smolianinov <97645771+Andrii-Smolianinov@users.noreply.github.com>
2023-10-23 02:11:48 +0300

    start Redux-toolkit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 import "./global.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(store);
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
