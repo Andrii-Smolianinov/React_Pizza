2490d841c0e743bdcc9228935f804f29ea23c4f7
Andrii-Smolianinov <97645771+Andrii-Smolianinov@users.noreply.github.com>
2023-10-26 21:41:19 +0300

    added axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 import Container from "./Container";
 import Header from "./components/Header/Header";
@@ -24,21 +25,13 @@ function App() {
     const sortBy = filterCategory ? `sortBy=${filterCategory}` : "";
 
     setIsLoading(true);
-    fetch(
-      `https://64fad951cb9c00518f7a461b.mockapi.io/items?${category}&${sortBy}`,
-      {
-        method: "GET",
-        headers: { "content-type": "application/json" },
-      }
-    )
+
+    axios
+      .get(
+        `https://64fad951cb9c00518f7a461b.mockapi.io/items?${category}&${sortBy}`
+      )
       .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        console.log("response error", res.status);
-      })
-      .then((data) => {
-        setItemsData(data);
+        setItemsData(res.data);
         setIsLoading(false);
       })
       .catch((error) => {
