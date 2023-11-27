77d938e03bf9f30579fc3a4cf8c83170620ff6fb
Andrii-Smolianinov <97645771+Andrii-Smolianinov@users.noreply.github.com>
2023-11-27 23:55:56 +0200

    added FullPizza

--- src/components/FullPizzaComponent.jsx
+++ src/components/FullPizzaComponent.jsx
@@ -2,16 +2,17 @@ import React, { useEffect, useState, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import ButtonComeBack from "./ButtonComeBack";
+import ImagePizza from "../assets/img/id-not-found.png";
 
 const FullPizzaComponent = () => {
-  const shouldLog = useRef(true);
+  const showDoubleRender = useRef(true);
   const [pizza, setPizza] = useState();
   const { id } = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (shouldLog.current) {
-      shouldLog.current = false;
+    if (showDoubleRender.current) {
+      showDoubleRender.current = false;
 
       async function fetchPizza() {
         try {
@@ -20,7 +21,7 @@ const FullPizzaComponent = () => {
           );
           setPizza(data);
         } catch (error) {
-          alert("Такої піци не існує!");
+          alert("Ооу, не чуди! Такого id піци не існує");
           navigate("/");
         }
       }
@@ -30,20 +31,37 @@ const FullPizzaComponent = () => {
   }, []);
 
   if (!pizza) {
-    return "Loading";
+    return "Loading...";
   }
 
   return (
     <div>
-      <ButtonComeBack tittle={"Назад до головного меню"} />
-      <div className="flex">
-        <div>
-          <img src={pizza.images} alt="PizzaImage"  className="max-w-[500px] max-h-[335px]"/>
-          <h2>{pizza.tittle}</h2>
+      <div className="my-4">
+        <ButtonComeBack tittle={"Назад до головного меню"} />
+      </div>
+      <div className="flex justify-start mt-4">
+        <div className="relative h-[335px]">
+          <img
+            src={pizza.images}
+            alt="PizzaImage"
+            className="block w-[500px] max-h-[335px]"
+          />
+          <div className="absolute w-[500px] h-1/5 bottom-0 bg-slate-800 opacity-80 ">
+            <h2 className="text-center font-bold text-2xl uppercase pt-4 tracking-wider text-sky-400">
+              {pizza.tittle}
+            </h2>
+          </div>
         </div>
         <div>
-          <p>{pizza.description}</p>
+          <p className="max-w-[800px] font-mono font-semibold text-justify ml-14 mr-8">
+            {pizza.description}
+          </p>
         </div>
+        <img
+          src={ImagePizza}
+          alt="PizzaImage"
+          className="block w-[300px] max-h-[335px]"
+        />
       </div>
     </div>
   );
--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import FullPizzaComponent from "../components/FullPizzaComponent";
 
+import { AppContext } from "../App";
+
 const FullPizza = () => {
+  const { setShowSearch } = React.useContext(AppContext);
+
+  React.useEffect(() => {
+    setShowSearch(false);
+  });
+
   return <FullPizzaComponent />;
 };
 
