84b70ab93c582eb0ef3a92a8b34d23798d3db39c
Andrii-Smolianinov <97645771+Andrii-Smolianinov@users.noreply.github.com>
2024-02-12 23:55:12 +0200

    added TS

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,8 +7,6 @@ import ChangeLanguage from "./ChangeLanguage";
 import logoJPG from "../../assets/img/newlogo.png";
 
 export default function Header({ showSearch, showButtonCart }) {
-  // const url = "javascript: alert('Hack from <a href={url} />');";
-
   return (
     <header
       className="flex flex-wrap items-center justify-between
@@ -28,10 +26,6 @@ export default function Header({ showSearch, showButtonCart }) {
           <span className="text-red-700">zza</span>
         </span>
       </Link>
-
-      {/* <img onError={alert("Hacked!")} src="invalid-image" /> */}
-       
-      {/* <a href={url}>Click to hack</a> */}
 
       {showSearch && <SearchPizza />}
 
